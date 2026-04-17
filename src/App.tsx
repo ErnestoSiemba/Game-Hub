a934a24ac8b6e45baf74bf0e4a10a74c92ee5aad
Ernesto Junior <siembaerney@yahoo.com>
2026-04-17 16:56:37 +0300

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Button colorPalette={"teal"} variant={"solid"}>
+        Hello World
+      </Button>
+    </>
+  );
+}
+
+export default App;
